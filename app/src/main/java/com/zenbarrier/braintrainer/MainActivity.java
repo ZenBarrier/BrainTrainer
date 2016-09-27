3ccee3e351ccc3bd8c6bb332d83517856580768c
ZenBarrier <anthony.j.barrera@gmail.com>
2016-09-27 18:49:22 -0400

    Game Complete

--- app/src/main/java/com/zenbarrier/braintrainer/MainActivity.java
+++ app/src/main/java/com/zenbarrier/braintrainer/MainActivity.java
@@ -1,11 +1,14 @@
 package com.zenbarrier.braintrainer;
 
+import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,12 +20,26 @@ public class MainActivity extends AppCompatActivity {
     View startButton;
     View gameLayout;
 
+    int sum;
+    int questionCount;
+    int score;
+
+    boolean isGameInProgress;
 
     final long GAME_LENGTH = 30;
 
     public void startGame(View view){
         startButton.setVisibility(View.GONE);
         gameLayout.setVisibility(View.VISIBLE);
+        final TextView feedbackText = (TextView)findViewById(R.id.feedbackText);
+        feedbackText.setText("");
+
+        ((TextView)findViewById(R.id.scoreText)).setText("");
+
+        isGameInProgress = true;
+
+        score = 0;
+        questionCount = 0;
 
         final View retryButton = findViewById(R.id.retryButton);
         retryButton.setVisibility(View.INVISIBLE);
@@ -40,26 +57,50 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 timerText.setText("0s");
+                isGameInProgress = false;
+                int percentRight = (int)((double)score/questionCount*100);
+                feedbackText.setText("Your score: "+ percentRight +"%");
+                feedbackText.setTextColor(Color.DKGRAY);
+                feedbackText.setAlpha(1f);
                 retryButton.setVisibility(View.VISIBLE);
             }
         }.start();
     }
 
     public void submitAnswer(View view){
+        if(isGameInProgress) {
+            int submittedAnswer = Integer.parseInt(((Button) view).getText().toString());
+            questionCount++;
+            TextView feedbackText = (TextView) findViewById(R.id.feedbackText);
+            if (submittedAnswer == sum) {
+                score++;
+                feedbackText.setText("Correct!");
+                feedbackText.setTextColor(Color.GREEN);
+            } else {
+                feedbackText.setText("Wrong");
+                feedbackText.setTextColor(Color.RED);
+            }
+            feedbackText.setAlpha(1f);
+            feedbackText.animate().alpha(0f).setDuration(1000);
 
+            TextView scoreText = (TextView) findViewById(R.id.scoreText);
+            scoreText.setText(score + "/" + questionCount);
+
+            createEquation();
+        }
     }
 
     void createEquation(){
         Random randomGenerator = new Random();
         int leftAddend = randomGenerator.nextInt(30);
         int rightAddend = randomGenerator.nextInt(30);
-        int sum = leftAddend + rightAddend;
+        sum = leftAddend + rightAddend;
 
         ArrayList answers = new ArrayList();
         answers.add(sum);
 
         while(answers.size() < 4){
-            int offset = randomGenerator.nextInt(21) - 10;
+            int offset = randomGenerator.nextInt(11) - 5;
             int wrongAnswer = sum + offset;
             if(!answers.contains(wrongAnswer)){
                 answers.add(wrongAnswer);
@@ -67,14 +108,14 @@ public class MainActivity extends AppCompatActivity {
         }
         Collections.shuffle(answers);
 
-        Button answerButton1 = (Button)findViewById(R.id.answerButton1);
-        answerButton1.setText(answers.get(0)+"");
-        Button answerButton2 = (Button)findViewById(R.id.answerButton2);
-        answerButton2.setText(answers.get(1)+"");
-        Button answerButton3 = (Button)findViewById(R.id.answerButton3);
-        answerButton3.setText(answers.get(2)+"");
-        Button answerButton4 = (Button)findViewById(R.id.answerButton4);
-        answerButton4.setText(answers.get(3)+"");
+        TableLayout answerTable = (TableLayout)findViewById(R.id.answerTable);
+        for(int i = 0 ; i < answerTable.getChildCount(); i++){
+            TableRow answerRow = (TableRow) answerTable.getChildAt(i);
+            for(int j =0 ; j < answerRow.getChildCount(); j++){
+                Button answerButton = (Button) answerRow.getChildAt(j);
+                answerButton.setText(answers.get(i+j*2)+"");
+            }
+        }
 
         TextView questionText = (TextView)findViewById(R.id.questionText);
         questionText.setText(leftAddend+" + "+rightAddend);
