41b954a085270c990df4e4e7348d70feb6f9fe72
ZenBarrier <anthony.j.barrera@gmail.com>
2016-09-27 15:58:38 -0400

    Game Start button

--- app/src/main/java/com/zenbarrier/braintrainer/MainActivity.java
+++ app/src/main/java/com/zenbarrier/braintrainer/MainActivity.java
@@ -2,12 +2,27 @@ package com.zenbarrier.braintrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    View startButton;
+    View gameLayout;
+
+    public void startGame(View view){
+        startButton.setVisibility(View.GONE);
+        gameLayout.setVisibility(View.VISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+        gameLayout = findViewById(R.id.gameLayout);
+
+        gameLayout.setVisibility(View.GONE);
+        startButton.setVisibility(View.VISIBLE);
     }
 }
